0424df0dca24c1acc65696eff8178d6aaa808fe0
Vishal5205 <vishalaws87@gmail.com>
2026-03-31 14:48:35 +0530

    Added Block device

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,6 +5,12 @@ resource "aws_instance" "EC2" {
     subnet_id = var.subnet_id
     vpc_security_group_ids = [var.sg_id]
     associate_public_ip_address = true
+
+    # Block Device Mapping
+    root_block_device {
+        volume_size = 20
+        volume_type = "gp3"
+    }
     tags = {
         Name = "Terraform_EC2"
     }
